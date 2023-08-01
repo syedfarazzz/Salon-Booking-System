a286d66e046040cfe47c40aee8733a69dad9a586
Muhammad Umar <umarkhan_96@outlook.com>
2023-08-01 18:12:38 -0500

    Fixing unrelated history issue

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: " https://0fa0-113-203-241-37.ngrok-free.app",
+  url: "https://api.everlybeauty.ca",
   app: {
     keys: env.array("APP_KEYS"),
   },
