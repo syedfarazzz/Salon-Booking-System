b25575fc999dc5d7c37d0539fa002edb68662846
Syed Faraz <syedfarazahmed.8@gmail.com>
2023-10-27 01:15:42 +0500

    Custom Response added

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,64 +1,68 @@
 const user = require('./content-types/user');
 
-module.exports = (plugin) => 
-{    
+module.exports = (plugin) => {
 
-    plugin.controllers.user.studentDiscount = async (ctx) => 
-    {
+    plugin.controllers.user.studentDiscount = async (ctx) => {
         const { isStudent } = ctx.request.body.data;
         const id = ctx.request.params.id;
 
-        if (isStudent === true)
-        {
+        if (isStudent === true) {
             var user = await strapi.entityService.findOne('plugin::users-permissions.user', id);
-            
-            if (user.isStudent === false) 
-            {
+
+            if (user.isStudent === false) {
                 const updatedCredits = +user.studentCredit + 50;
 
-                var res = await strapi.entityService.update('plugin::users-permissions.user', id, 
-                {
-                    data: 
+                var res = await strapi.entityService.update('plugin::users-permissions.user', id,
                     {
-                        studentCredit: updatedCredits,
-                        isStudent: true
-                    } 
-                });
-                
-                ctx.body = res;
+                        data:
+                        {
+                            studentCredit: updatedCredits,
+                            isStudent: true
+                        }
+                    });
+
+                // Create a custom response object with your message
+                var customResponse = {
+                    message: "Congratulations!",
+                    data: res // Include the existing response data
+                };
+
+                ctx.body = customResponse;
+
+                // ctx.body = res;
             }
-            else{
+            else {
                 return ctx.body = { message: 'Customer has already availed Student Discount' };
             }
 
-            
+
         }
-        else{
-            return ctx.body = { message:'Kindly Select the discount' };
+        else {
+            return ctx.body = { message: 'Kindly Select the discount' };
         }
-        
-            
-        
+
+
+
     }
 
     plugin.contentTypes.user = user;
 
     // Routes
-        /**
-        Adds a POST method route that is handled by the studentDiscount function above.
-        **/
-        plugin.routes['content-api'].routes.push({
-                method: 'POST',
-                path: '/users/student/:id',
-                handler: 'user.studentDiscount',
-                config: {
-                    prefix: '',
-                    policies: []
-                }
-        },
-        );
+    /**
+    Adds a POST method route that is handled by the studentDiscount function above.
+    **/
+    plugin.routes['content-api'].routes.push({
+        method: 'POST',
+        path: '/users/student/:id',
+        handler: 'user.studentDiscount',
+        config: {
+            prefix: '',
+            policies: []
+        }
+    },
+    );
 
-    
+
 
     return plugin;
 }
